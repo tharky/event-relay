5d175206c2e1dbdbe619396d11e818a5153e28a5
tharkins <tharkins@wisc.edu>
2026-09-21 20:07:27 -0500

    initial spring boot backend commit

---
+++ backend/src/main/java/com/tigerharkins/event_relay/EventRelayApplication.java
@@ -0,0 +1,12 @@
+package com.tigerharkins.event_relay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventRelayApplication.class, args);
+    }
+}
